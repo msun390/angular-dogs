c3c442bc2ea6605fb52be5a7d6f07804bb0e5331
msun390 <114260433+msun390@users.noreply.github.com>
2022-09-29 12:11:25 -0400

    1 file modified

--- src/app/mock-dogs.ts
+++ src/app/mock-dogs.ts
@@ -1,15 +1,15 @@
 import { Dog } from './dog';
 
 export const DOGS: Dog[] = [
-  { id: 12, name: 'Akita', personality: 'Quite, but quick' },
-  { id: 13, name: 'Beagle', personality: 'Gentle, sweet, and funny' },
-  { id: 14, name: 'Chihuahua', personality: 'Feisty and outgoing'  },
-  { id: 15, name: 'Dalmatian', personality: 'Energetic, playful, and sensitive'  },
-  { id: 16, name: 'English Bulldog', personality: 'Sweet and gentle'  },
-  { id: 17, name: 'French Bulldog', personality: 'Stubborn and lovely'  },
-  { id: 18, name: 'Golden Retriever', personality: 'Playful, yet gentle'  },
-  { id: 19, name: 'Husky', personality: 'Intelligent, playful, and very friendly'  },
-  { id: 20, name: 'Irish Setter', personality: 'Happy, affectionate, and eager to please'  }
+  { id: 1, name: 'Akita', personality: 'Quite, but quick' },
+  { id: 2, name: 'Beagle', personality: 'Gentle, sweet, and funny' },
+  { id: 3, name: 'Chihuahua', personality: 'Feisty and outgoing'  },
+  { id: 4, name: 'Dalmatian', personality: 'Energetic, playful, and sensitive'  },
+  { id: 5, name: 'English Bulldog', personality: 'Sweet and gentle'  },
+  { id: 6, name: 'French Bulldog', personality: 'Stubborn and lovely'  },
+  { id: 7, name: 'Golden Retriever', personality: 'Playful, yet gentle'  },
+  { id: 8, name: 'Husky', personality: 'Intelligent, playful, and very friendly'  },
+  { id: 9, name: 'Irish Setter', personality: 'Happy, affectionate, and eager to please'  }
 ];
 
 
